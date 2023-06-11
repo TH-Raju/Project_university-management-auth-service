7dbaa513037aedcfaa0dbecee4a961cd400a14a7
TH-Raju <rajukhan894200@gmail.com>
2023-06-11 20:35:41 +0600

    [Done] created Academic Semester Interface, Model

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,25 @@
-import cors from 'cors'
-import express, { Application } from 'express'
-import globalErrorHandler from './app/middlewares/globalErrorHandler'
-import { UserRoutes } from './app/modules/user/user.route'
-const app: Application = express()
+import cors from 'cors';
+import express, { Application } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import { UserRoutes } from './app/modules/user/user.route';
+const app: Application = express();
 
-app.use(cors())
+app.use(cors());
 
 //parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Application routes
 // console.log(app.get('env'));
 
-app.use('/api/v1/users/', UserRoutes)
+app.use('/api/v1/users/', UserRoutes);
 
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
 //   console.log(x);
 // })
 
 // global error handler
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
-export default app
+export default app;
--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,19 +1,19 @@
-import { User } from './user.model'
+import { User } from './user.model';
 
 export const findLastUserId = async () => {
   const lastUser = await User.findOne({}, { id: 1, _id: 0 })
     .sort({
       createdAt: -1,
     })
-    .lean()
+    .lean();
 
-  return lastUser?.id
-}
+  return lastUser?.id;
+};
 
 export const generateUserId = async () => {
-  const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0')
+  const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0');
 
   //increment by 1
-  const incrementId = (parseInt(currentId) + 1).toString().padStart(5, '0')
-  return incrementId
-}
+  const incrementId = (parseInt(currentId) + 1).toString().padStart(5, '0');
+  return incrementId;
+};
